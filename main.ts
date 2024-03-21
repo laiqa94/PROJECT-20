89f67ba51e819c0d978398c0a0bdd42c2b5f1233
laiqa khan <152205933+laiqa94@users.noreply.github.com>
2024-03-22 02:15:07 +0500

    Add files via upload
    THIS IS MY PROJECT

---
+++ main.ts
@@ -0,0 +1,41 @@
+
+let Guest_list :string[] =['kamran tassori','Daniyal nagori','Zia khan'];
+// for(let i=0; i<Guest_list.length; i++){
+//    console.log('Dear mr.' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in our party'.\n\nThank you!\n\n)
+
+//}
+
+let absant_Guest :String ='Daniyal nagori';
+let new_Guest :string='Ameen alam';
+Guest_list[0]= new_Guest ;
+
+//for(let i=0; i<Guest_list.length; i++){
+//    console.log('Dear mr.' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in our party'.\n\nThank you!\n\n)
+    
+//}
+
+//console.log(`Mr. ${absant_Guest} is not coming to the party.`);
+//console.log('Good New! We find Big Table so we are inviting 3 more guest.')
+
+Guest_list.unshift('hamza syed');
+Guest_list.splice(2 , 0 ,'Qasim');
+Guest_list.push('Hira khan');
+
+//for(let i=0; i<Guest_list.length; i++){
+//   console.log('Dear Mr.' + Guest_list[1] +',/n/nIt is our pleasure o invite you in our party.\n\nThank you!\n\n)
+
+//}
+
+while(Guest_list.length> 2){
+  let remove_Guest =Guest_list.pop();
+  //console.log(`Sorry Mr. ${remove_Guest}, You are not invited for Dinner.`);
+}
+
+//for(let i=0; i<Guest_list.length; i++){
+   // console.log('Dear Mr.' + Guest_list[i] + ',\n\nYou are still invited.\n\nThank you!\n\n')
+//}
+
+Guest_list.splice(0 ,2);
+console.log(Guest_list);
+
+console.log(`Total number of Guest Are: ${Guest_list.length}`);
